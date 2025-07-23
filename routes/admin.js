fcee73e670137620c7c417b173f1ddf51393bf71
sunah288 <qq77273899@gmail.com>
2025-07-23 13:31:19 +0900

    이미지 삭제2

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,8 @@
 import express from 'express';
 import {authMiddleware} from '../middlewares/authMiddleware.js';
 import isAdmin from '../middlewares/isAdmin.js';
-import upload from '../middlewares/cloudinaryUploader.js';
-import { getDashboardStats, getAllUsers } from '../controllers/adminController.js';
-import { uploadImage, getImages } from '../controllers/adminController.js';
+import upload from '../middlewares/cloudinaryUploader.js'; // ✅← 추가할 부분
+import { getDashboardStats, getAllUsers,uploadImage, getImages, deleteImage } from '../controllers/adminController.js';  // ✅✅ ← deleteImage 추가할 부분
 
 const router = express.Router();
 
@@ -12,8 +11,9 @@ router.use(authMiddleware, isAdmin); // 인증 + 관리자 권한 필수
 
 router.get('/dashboard', getDashboardStats);
 router.get('/users', getAllUsers);
-router.post('/upload-image', upload.single('file'), uploadImage);         // ✅✅✅ ← 추가할 부분
-router.get('/images', getImages);  
+router.post('/upload-image', upload.single('file'), uploadImage);         // ✅← 추가할 부분
+router.get('/images', getImages);                  // ✅ ← 추가할 부분
+
+router.post('/delete-image', deleteImage); // ✅✅ ←  POST 요청으로 업로드 된 이미지 삭제
 
 export default router;
-// 
