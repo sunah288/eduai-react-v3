9f41875dc7c4dea1ecebd9db7339e6f51ed17b52
sunah288 <qq77273899@gmail.com>
2025-07-24 09:48:02 +0900

    동영상 업로드 수정

--- middlewares/cloudinaryUploader.js
+++ middlewares/cloudinaryUploader.js
@@ -1,27 +1,24 @@
-// server/middleware/cloudinaryUploader.js
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 import cloudinary from '../config/cloudinary.js';
 
-const storage = new CloudinaryStorage({
+// 이미지 업로드 설정
+const imageStorage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'admin-uploads',
     allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
   },
 });
+export const upload = multer({ storage: imageStorage });
 
-const upload = multer({ storage });
-
+// 영상 업로드 설정
 const videoStorage = new CloudinaryStorage({
   cloudinary,
   params: {
-    resource_type: 'video',              // ✅ 영상 전용
+    resource_type: 'video',
     folder: 'admin-videos',
     allowed_formats: ['mp4', 'mov', 'webm', 'mkv'],
   },
 });
-
-const uploadVideo = multer({ storage: videoStorage });
-
-export default upload;
+export const uploadVideo = multer({ storage: videoStorage });
